3695887221be8582cdbfb81795dd6fe379c79fb6
Jacob_str <80145275+Ghdhdhdh@users.noreply.github.com>
2024-04-29 14:29:44 +1000

    Update main.py
    added mammal

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     #get info about animal
     teeth = str(input("How many teeth does your animal have? "))
     wild = str(input("Is the animal wild? "))
+    mammal = str(input("Is the animal a mammal? "))
     #Find ANimal
     with open("animals.json") as jsonFile:
         data = json.load(jsonFile)
@@ -12,7 +13,7 @@ def main():
         #print(e)
         e2 = data[e]
         #print(e2)
-        if teeth == e2["teeth"] and wild == e2["wild"]:
+        if teeth == e2["teeth"] and wild == e2["wild"] and mammal == e2["mammal"]:
             print("Youre thinking of a " + e)
 
 
